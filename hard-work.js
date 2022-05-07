fb7f677102afe86f1bd4157ef382f188ebc6abad
VituuD2 <overroot0@gmail.com>
2022-05-06 23:53:12 -0300

    kyu-tv 6-5-22 5

--- hard-work.js
+++ hard-work.js
@@ -11,3 +11,11 @@ labelLogo.addEventListener('mouseleave', ()=>{
     secondLogo.classList.add('hide-logo');
     mainLogo.classList.remove('hide-logo');
 });
+
+
+let bars_btn = document.querySelector('.bars-ico');
+let ul = document.querySelector('.ul');
+
+bars_btn.addEventListener('click', ()=>{
+    ul.classList.toggle('ul-right');
+});
